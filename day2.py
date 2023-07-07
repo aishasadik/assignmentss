c1e8b5280d2a9779ba03fc35b5250e8a2132a74b
aishasadik <138862030+aishasadik@users.noreply.github.com>
2023-07-07 20:39:45 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,78 @@
+# arithmetic operator
+a=20
+b=10
+print("Addition: ",a+b)
+print("Subtraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
+print("Mod: ",a%b)
+print("Floor Division: ",a//b)
+print("Exponentation: ",a**b)
+
+# assignment operator
+a=5
+a+=2
+print("+=",a)
+a=5 ; 
+a-=2
+print("-=",a)
+a=5
+a*=2
+print("*=",a)
+a=5
+a/=2
+print("/=",a)
+a=5
+a//=2
+print("//=",a)
+a=5
+a%=2
+print("%=",a)
+a=5
+a**=2
+print("**=",a)
+
+# comparision operator
+a=20
+b=10
+print("20>=10",a>=b)
+print("20<=10",a<=b)
+print("20==10",a==b)
+print("20!=10",a!=b)
+print("20<10",a<b)
+print("20>10",a>b)
+
+# logical operator
+a=True
+b=False
+print("and",a and b)
+print("or",a or b)
+print("not",not b)
+
+
+# bitwise operator
+a=20
+b=10
+print("bitwise &",a&b)
+print("bitwise |",a|b)
+print("bitwise ^",a^b)
+print("bitwise >>",a>>2)
+print("bitwise <<",a<<2)
+
+#identity operator
+a=20
+b=20.0
+print("is",a is b)
+print("is not",a is not b)
+
+# membership operators
+a="aisha"
+print("in","s" in a)
+print("not in","S" not in a)
+
+
+
+
+
+
+
